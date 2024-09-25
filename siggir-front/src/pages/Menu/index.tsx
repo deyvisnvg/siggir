ff86ca40d773b383c7ee80b97f74d838bd82cdc7
deyvisnvg <deyvis.valdez_consultor@mdp.com.pe>
2024-09-25 01:42:22 -0500

    feat(deyvisnvg): Correction

--- siggir-front/src/pages/Menu/index.tsx
+++ siggir-front/src/pages/Menu/index.tsx
@@ -3,20 +3,18 @@ import SidebarIcon from "./components/SidebarIcon";
 
 export default function Menu() {
     return (
-        <>
-            <div className="flex">
-                <div>
-                    <div className="hidden md:inline-block">
-                        <SidebarMenu />
-                    </div>
-                    <div className="hidden max-md:inline-block">
-                        <SidebarIcon />
-                    </div>
+        <div className="flex">
+            <div>
+                <div className="hidden md:inline-block">
+                    <SidebarMenu />
                 </div>
-                <div className="flex justify-center items-center w-full">
-                    Contenido
+                <div className="hidden max-md:inline-block">
+                    <SidebarIcon />
                 </div>
             </div>
-        </>
+            <div className="flex justify-center items-center w-full">
+                Contenido
+            </div>
+        </div>
     )
 }
