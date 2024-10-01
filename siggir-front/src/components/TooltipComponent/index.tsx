0c950ae97dae2fa918d62ac5cfbba6f970c5c862
deyvisnvg <deyvis.valdez_consultor@mdp.com.pe>
2024-10-01 01:29:34 -0500

    feat(deyvisnvg): Updated Tooltip Component

--- siggir-front/src/components/TooltipComponent/index.tsx
+++ siggir-front/src/components/TooltipComponent/index.tsx
@@ -2,15 +2,17 @@ import { Tooltip, TooltipAction, TooltipContent } from 'keep-react'
 import { ReactNode } from 'react';
 
 interface Props {
-    children: ReactNode
+    children: ReactNode;
+    textContent: string;
+    positionTooltip: any
 }
 
-const TooltipComponent = ({ children }: Props) => {
+const TooltipComponent = ({ children, textContent, positionTooltip }: Props) => {
     return (
-        <Tooltip placement="right">
-            <TooltipAction className=''>{children}</TooltipAction>
+        <Tooltip placement={positionTooltip}>
+            <TooltipAction>{children}</TooltipAction>
             <TooltipContent>
-                <p className="text-body-5 font-medium text-white">Despliegue Opciones</p>
+                <p className="text-body-5 font-medium text-white">{textContent}</p>
             </TooltipContent>
         </Tooltip>
     )
