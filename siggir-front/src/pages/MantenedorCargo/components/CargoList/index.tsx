f138a5e06d37c6dcd57185dce1de15f5ebb8ee6d
deyvisnvg <deyvis.valdez_consultor@mdp.com.pe>
2024-10-06 21:32:15 -0500

    feat(deyvisnvg): Updated CargoList component

---
+++ siggir-front/src/pages/MantenedorCargo/components/CargoList/index.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import { Plus } from 'phosphor-react'
+import {
+    DropdownItem,
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from 'keep-react';
+import { EllipsisHorizontalCircleIcon } from '@heroicons/react/24/outline';
+import { ButtonComponent, DropdownComponent, ModalComponent, Pagination, SearchBar } from '@/components';
+import { useContext } from 'react';
+import { MyContext } from '@/contexts';
+import CargoAdd from '../CargoAdd';
+
+export default function CargoList() {
+    const context = useContext(MyContext);
+
+    if (!context) {
+        throw new Error('UserList debe usarse dentro de un PaginationSearchProvider');
+    }
+
+    const { current, handlePageChange, pageCount, searchTerm, setSearchTerm } = context;
+
+    return (
+        <>
+            <Table>
+                <TableCaption>
+                    <div className="flex flex-col items-center gap-5">
+                        <div className="">
+                            <h2 className="text-heading-6 font-semibold text-metal-900 dark:text-white">Mantenimiento Cargo</h2>
+                        </div>
+                        <div className="flex justify-between gap-5 w-full ">
+                            <ModalComponent
+                                formModal={<CargoAdd />}
+                                titleModal="Registrar Cargo"
+                            >
+                                <div>
+                                    <ButtonComponent
+                                        iconButton={Plus}
+                                        size="sm"
+                                        text="Registrar"
+                                        color="success"
+                                    />
+                                </div>
+                            </ModalComponent>
+                            <SearchBar
+                                placeholder="Buscar..."
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </TableCaption>
+                <TableHeader>
+                    <TableRow className='*:bg-gray-600 *:text-white'>
+                        <TableHead className='rounded-s-lg'>
+                            <div className="w-auto">Nombre del Cargo</div>
+                        </TableHead>
+                        <TableHead className='text-center rounded-e-md'>
+                            <div className="w-auto">Acción</div>
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {current.map((item: any) => (
+                        <TableRow key={item.id}>
+                            <TableCell>{item.cargo}</TableCell>
+                            <TableCell className='flex justify-around'>
+                                <DropdownComponent
+                                    iconButtonDropdown={
+                                        <span className={
+                                            `hint--left
+                                            hint--no-arrow 
+                                            hint--rounded hover:text-green-700 size-6 cursor-pointer`}
+                                            aria-label="Más Opciones"
+                                        >
+                                            <EllipsisHorizontalCircleIcon />
+                                        </span>
+                                    }
+                                    positionDropdown='top'
+                                >
+                                    <DropdownItem className='focus:outline-0'>Visualizar</DropdownItem>
+                                    <DropdownItem>Editar</DropdownItem>
+                                </DropdownComponent>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <Pagination
+                pageCount={pageCount}
+                onPageChange={handlePageChange}
+            />
+        </>
+    )
+}
