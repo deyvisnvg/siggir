592712a526f5676c791f28d5818ef71eb6691d9d
deyvisnvg <deyvis.valdez_consultor@mdp.com.pe>
2024-10-17 09:15:44 -0500

    feat(deyvisnvg): the MantenedorAsigUsuario component was implemented

---
+++ siggir-front/src/pages/MantenedorAsigUsuario/components/index.ts
@@ -0,0 +1,3 @@
+export { default as AsigUsuarioList } from "./AsigUsuarioList";
+export { default as AsigUsuarioAdd } from "./AsigUsuarioAdd";
+export { default as AsigUsuarioEdit } from "./AsigUsuarioEdit";
