0b9e4d7bd4503f62d6eb097517f395eb8ad3d79a
deyvisnvg <deyvis.valdez_consultor@mdp.com.pe>
2024-10-17 09:20:17 -0500

    feat(deyvisnvg): updated

--- siggir-front/src/pages/MantenedorGerencia/components/GerenciaAdd/index.tsx
+++ siggir-front/src/pages/MantenedorGerencia/components/GerenciaAdd/index.tsx
@@ -4,12 +4,14 @@ export default function GerenciaAdd() {
     return (
         <form action="" className="px-2">
             <div className="flex flex-col gap-y-3 pt-3.5">
-                <label htmlFor="gerencia" className="text-sm font-medium">Nombre de gerencia</label>
-                <input type="text"
-                    id="gerencia"
-                    className="border border-gray-400 rounded-md px-3 py-1.5 text-sm"
-                    placeholder="Ingrese gerencia"
-                />
+                <div className="flex flex-col gap-y-0.5">
+                    <label htmlFor="gerencia" className="text-sm font-medium">Nombre de gerencia</label>
+                    <input type="text"
+                        id="gerencia"
+                        className="border border-gray-400 rounded-md px-3 py-1.5 text-sm"
+                        placeholder="Ingrese gerencia"
+                    />
+                </div>
             </div>
             <div className="text-center pt-7">
                 <ButtonComponent
